f97e11c2e17fa92c184423d199b0affb44e91348
ElementalMP4 <sebahudl@gmail.com>
2024-09-25 18:17:40 +0100

    Fix installer

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-SCRIPT_PATH="./quickstack.py"
-INSTALL_DIR="/usr/local/bin"
-SCRIPT_NAME=$(basename "$SCRIPT_PATH")
-DEST_PATH="$INSTALL_DIR/$SCRIPT_NAME"
-
-if [ ! -w "$INSTALL_DIR" ]; then
-    echo "You do not have write permission to '$INSTALL_DIR'. Try running with sudo."
+if [ ! -w "/usr/local/bin" ]; then
+    echo "You do not have write permission to '/usr/local/bin'. Try running with sudo."
     exit 1
 fi
 
 echo "Installing $SCRIPT_NAME to $INSTALL_DIR..."
-cp "$SCRIPT_PATH" "$DEST_PATH"
+cp "./quickstack.py" "/usr/local/bin/quickstack"
 
-chmod +x "$DEST_PATH"
+chmod +x "/usr/local/bin/quickstack"
 
 if [ $? -eq 0 ]; then
     echo "Installation successful!"
