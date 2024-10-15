ff715cd2612cfffc6cf1d3de0b5c8f4bf9fd1ffb
ElementalMP4 <sebahudl@gmail.com>
2024-10-15 20:09:18 +0100

    Change alias

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 INSTALL_DIR=
 
 if [ $OSTYPE == "linux-gnu" ]; then
-    INSTALL_DIR="/usr/local/bin/quickstack"
+    INSTALL_DIR="/usr/local/bin/qs"
 else
-    INSTALL_DIR=$HOME/bin/quickstack
+    INSTALL_DIR=$HOME/bin/qs
 fi
 
 mkdir -p $HOME/bin
